61c7072837abd6b51811a349e45f0088d02ab5cc
Nazar Bilinskyi <62640154+n-bilinskyi@users.noreply.github.com>
2026-04-17 12:25:58 +0300

    oplus_setup.sh: use oplus6989v2.config overlay

--- kernel/oplus/build/oplus_setup.sh
+++ kernel/oplus/build/oplus_setup.sh
@@ -5,7 +5,7 @@ function init_build_environment() {
     export CHIPSET_COMPANY=MTK
     export OPLUS_VND_BUILD_PLATFORM=MT6989
     export BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1
-    export DEFCONFIG_OVERLAYS="fpga.config"
+    export DEFCONFIG_OVERLAYS="fpga.config oplus6989v2.config"
     export KERNEL_VERSION=kernel-6.1
     export OPLUS_PLATFORM_INFO=k6989v1_64
     export OPLUS_FEATURES=""
